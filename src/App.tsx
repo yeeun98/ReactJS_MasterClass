4ea1ec19563e36461589e81bee62e872389c1c19
yeeun_kim_ <feb23rd@golfzon.com>
2024-07-03 11:01:33 +0900

    #4.0 - 4.1 : Router - intro & Browser Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import styled from 'styled-components';
-
-const Container = styled.div`
-  background-color: ${prop => prop.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${prop => prop.theme.txtColor};
-`;
+import React from "react";
+import Router from "./Router";
 
 function App() {
-
   return (
-    <Container>
-      <H1>protected</H1>
-    </Container>
+    <div>
+      <Router />
+    </div>
   );
 }
 
