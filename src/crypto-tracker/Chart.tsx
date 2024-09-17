76d4d2cb770ff847fa9f884a6eed94f2c1b6ea10
김예은 <dfeb23rd@gmail.com>
2024-09-18 01:11:31 +0900

    # 5.13 Price Chart part Two

--- src/crypto-tracker/Chart.tsx
+++ src/crypto-tracker/Chart.tsx
@@ -1,15 +1,66 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { fetchCoinHistory } from "./api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 
 function Chart() {
   const { coinId } = useParams();
-  const { isLoading, data } = useQuery({
+  const { isLoading, data } = useQuery<IHistorical[]>({
     queryKey: ['history'],
     queryFn: () => fetchCoinHistory(coinId ?? '')
   })
 
-  return <h1>Chart</h1>
+  return <div>
+    {
+      isLoading ? "Loading Chart..." :
+        <ApexChart 
+        type="line"
+        series={[
+          {
+            name:'sales',
+            data: data?.map(price => price.close) ?? []
+          },
+        ]}
+        options={{
+          theme: {
+            mode: 'dark'
+          },
+          chart: {
+            height: 300,
+            width: 500,
+            toolbar: {
+              show: false
+            },
+            background: 'transparent'
+          },
+          grid: {show: false},
+          stroke: {
+            curve: 'smooth',
+            width: 4
+          },
+          yaxis: {show: false},
+          xaxis: {
+            axisTicks: {
+              show: false
+            },
+            labels: {
+              show: false
+            }
+          },
+        }}/>
+    }
+  </div>
 }
 
 export default Chart;
